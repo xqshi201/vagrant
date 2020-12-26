30897d492705e85efbf56bdddc0391b68165f09c
xqshi <xqshi@i9i8.com>
2020-12-26 16:19:56 +0800

    1

--- init_php_environment.sh
+++ init_php_environment.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+echo "root password is :123456"
+sudo sleep 5s
+echo "123456" | sudo passwd root --stdin
+sudo yum update -y
+sudo yum install -y  kernel-devel
+sudo systemctl stop firewalld
+sudo sed -i "s/#PasswordAuthentication yes/PasswordAuthentication yes/" /etc/ssh/sshd_config
+sudo service sshd restart
+
+
