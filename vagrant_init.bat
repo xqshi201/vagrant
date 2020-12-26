d19a6c51b74ca03c123590131caf6a712de97a95
xqshi <xqshi@i9i8.com>
2020-12-26 15:50:13 +0800

    1

--- vagrant_init.bat
+++ vagrant_init.bat
@@ -1,9 +1,12 @@
+@echo off
 d:
 mkdir d:\vagrant_virtual
+cd d:\vagrant_virtual
 vagrant box add centos D:\vagrant_download\CentOS-7.box
 vagrant init centos
-xcopy  d:\vagrant_project\vagrant\Vagrantfile d:\vagrant_virtual\Vagrantfile /Y
-xcopy  d:\vagrant_project\vagrant\Vagrantfile\启动虚拟机.bat C:\Users\admin\Desktop\启动虚拟机.bat
-vagrant reload
+copy  d:\vagrant_project\vagrant\Vagrantfile d:\vagrant_virtual\Vagrantfile /Y
+copy  d:\vagrant_project\vagrant\Vagrantfile\启动虚拟机.bat C:\Users\admin\Desktop\启动虚拟机.bat /Y
+vagrant up
 vagrant ssh
+pause
 
